0a918b44e941c835ce8ef1918f1b1e63d73fe34c
alanmaranto <43775935+alanmaranto@users.noreply.github.com>
2019-09-01 22:48:08 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import HolaMundo from './components/HolaMundo';
+import Button from './components/Button';
+import * as serviceWorker from './serviceWorker';
+
+const texts= {
+    text: 'Click',
+    text2: 'Click2',
+    text3: 'Click3'
+}
+
+ReactDOM.render(<Button {...texts} />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
